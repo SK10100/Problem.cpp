7e62de202d35636383daf9d4db77ad6c44c04a8b
Sonali Kumari <sonali522003@gmail.com>
2024-03-15 23:56:53 +0530

    Headers.cpp
    The<climits>header contain information about limit of integer and <cfloat>  header contain information on limits for floating point numbers.

--- TestInteger.cpp
+++ TestInteger.cpp
@@ -1,6 +1,7 @@
 /*test integer din <climits> header */
 #include<iostream>
 #include<climits>
+#include<cfloat>
 using namespace std;
 int main(){
   cout<<"int max= "<<INT_MAX<<endl;
@@ -14,7 +15,15 @@ int main(){
   cout<<"signed Char max= "<<SCHAR_MAX<<endl;
   cout<<"signed Char min= "<<SCHAR_MIN<<endl;
   cout<<"unsined Char max= "<<UCHAR_MAX<<endl;
-
+  cout<<"unsigned int min= "<<SHRT_MIN<<endl;
+  cout<<"unsigmed long max= "<<ULONG_MAX<<endl;
+  cout<<"-----------------------NOW for <cfloat>------------------------"<<endl;
+  cout<<"float min integer Digits= "<<FLT_DIG<<endl;
+  cout<<"double min integer Digits= "<<DBL_DIG<<endl;
+  cout<<"long double min integer digits= "<<LDBL_DIG<<endl;
+  cout<<"float max integer digits= "<<FLT_MANT_DIG<<endl;
+  cout<<"double max integer digits= "<<DBL_MANT_DIG<<endl;
+  cout<<"long double max integer digitas= "<<LDBL_MANT_DIG<<endl;
   return 0;
 
 }
