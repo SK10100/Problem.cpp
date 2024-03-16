c1b3065971cbcfca11a16f32145739cc08e1b025
Sonali Kumari <sonali522003@gmail.com>
2024-03-16 17:40:53 +0530

    INT_Literals.cpp

--- IntegerLiterals.cpp
+++ IntegerLiterals.cpp
@@ -5,6 +5,10 @@ int main(){
   int number2=01234;
   int number3=0x1abc;
   int number4=0b1000101;
-  cout<<number1<<endl<<number2<<endl<<number3<<endl<<number4<<endl;
+  long number = 12345678L;
+  long long sum=123;
+  long long bigNUmber=987654321LL;
+  cout<<"NUmber in Decimal: \n"<<endl;
+  cout<<number1<<endl<<number2<<endl<<number3<<endl<<number4<<endl<<number<<sum<<endl<<bigNUmber<<endl;
    return 0;
 }
