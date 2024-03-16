ea0c2381ad11295d6fca685b6b4efe4168f876a0
Sonali Kumari <sonali522003@gmail.com>
2024-03-16 17:55:23 +0530

    INT_Literals-problem

--- IntegerLiterals.cpp
+++ IntegerLiterals.cpp
@@ -8,7 +8,18 @@ int main(){
   long number = 12345678L;
   long long sum=123;
   long long bigNUmber=987654321LL;
-  cout<<"NUmber in Decimal: \n"<<endl;
+  cout<<"NUmber in Decimal:\n";
   cout<<number1<<endl<<number2<<endl<<number3<<endl<<number4<<endl<<number<<sum<<endl<<bigNUmber<<endl;
+  cout<<"------------NOW float and char Literals--------------"<<endl;
+  //float average=55.66;
+  float average=55.66f;
+  cout<<average<<endl;
+  char letter='a';
+  char anotherLetter=98;
+  cout<<letter<<endl;
+  cout<<anotherLetter<<endl;
+  anotherLetter+=2;
+  cout<<anotherLetter<<endl;
+  cout<<(int)anotherLetter<<endl;
    return 0;
 }
