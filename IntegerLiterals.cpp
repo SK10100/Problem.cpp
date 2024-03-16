3cf868a5b6174bcae32b66be95180f292e7e4ccc
Sonali Kumari <sonali522003@gmail.com>
2024-03-16 17:33:21 +0530

    integerlterals.cpp
    A literals is a specific constant value,such as 123,-466,3.14,'a' ,"Hello".

---
+++ IntegerLiterals.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int number1=123;
+  int number2=01234;
+  int number3=0x1abc;
+  int number4=0b1000101;
+  cout<<number1<<endl<<number2<<endl<<number3<<endl<<number4<<endl;
+   return 0;
+}
