ab25c4320aea2a48c09b83ae9873122483a19c0d
Sonali Kumari <sonali522003@gmail.com>
2024-03-16 18:08:21 +0530

    ASCII_COde_Table

---
+++ ASCIICodeTable.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  char letter;
+  cout<<"Enter a alphabet: ";
+  cin>>letter;
+  cout<<"The ASCII Code Of alphabet is: "<<(int)letter<<endl;
+  return 0;
+
+}
