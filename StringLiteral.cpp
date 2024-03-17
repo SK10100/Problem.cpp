b4ab6a95f284a6cb1df454e9ac03832414e9321b
Sonali Kumari <sonali522003@gmail.com>
2024-03-17 12:45:57 +0530

    StringLitteral.cpp

---
+++ StringLiteral.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cctype>
+using namespace std;
+int main(){
+string directionMsg="Turn Right";
+string greetingMsg="Hello";
+string statusMsg="";
+cout<<directionMsg<<endl<<greetingMsg<<endl<<statusMsg<<endl;
+cout<<"use\\\"to place\nwhithin\ta\tstring"<<endl;
+cout<<"          '_____'\n          ( o o )\n  + ====== \\   /\n / ||% % %|| \n*  ||-----||\n   \"\"     \"\"     ";
+return 0;
+
+}
