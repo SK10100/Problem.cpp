3b05705d584aeef6e6ddad932c18e1987cf08ed2
Sonali Kumari <sonali522003@gmail.com>
2024-03-17 15:05:13 +0530

    StringLiteral.cpp

--- StringLiteral.cpp
+++ StringLiteral.cpp
@@ -7,6 +7,7 @@ string greetingMsg="Hello";
 string statusMsg="";
 cout<<directionMsg<<endl<<greetingMsg<<endl<<statusMsg<<endl;
 cout<<"use\\\"to place\nwhithin\ta\tstring"<<endl;
+cout<<"-------NOW Print the Cow picture ------------\n";
 cout<<"          '_____'\n          ( o o )\n  + ====== \\   /\n / ||% % %|| \n*  ||-----||\n   \"\"     \"\"     ";
 return 0;
 
---
+++ TestLiteral.cpp
@@ -0,0 +1,21 @@
+/*Testing primitive Types */
+#include<iostream>
+using namespace std;
+int main(){
+  char gender='m';
+  bool isMarried=true;
+  unsigned short numChildren=8;
+  short YearofBirth=1945;
+  unsigned int salary=88000;
+  double weight=88.88;
+  float gpa=3.88f;
+  cout<<"Gender is: "<<gender<<endl;
+  cout<<"IS Married?: "<<isMarried<<endl;
+  cout<<"Number of children is: "<<numChildren<<endl;
+  cout<<"Year of Birth: "<<YearofBirth<<endl;
+  cout<<"Salary: "<<salary<<endl;
+  cout<<"Weight: "<<weight<<endl;
+  cout<<"GPA; "<<gpa<<endl;
+  return 0;
+
+}
